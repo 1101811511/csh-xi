c63be27c1b21982c54da037c418974195cdc0fe3
chenlong <chenlong@chunmi.com>
2017-05-12 21:36:04 +0800

    1、init Commit。

---
+++ app/src/main/java/com/example/administrator/swipedemo/BaseActivity.java
@@ -0,0 +1,9 @@
+package com.example.administrator.swipedemo;
+
+import com.trello.rxlifecycle.components.support.RxAppCompatActivity;
+
+/**
+ * Created by Administrator on 2017/5/11.
+ */
+public class BaseActivity extends RxAppCompatActivity {
+}
